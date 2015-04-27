6c622fefbdf6ae276d2a5504fa50dce0d5e66ff3
dtrujo <dtrujo@gmail.com>
2015-04-27 15:17:48 +0100

    Add middleware to add headers

--- server.js
+++ server.js
@@ -17,9 +17,18 @@ mongoose.connect('mongodb://localhost/wodapp', function(err, res){
  * Middlewares
  */
 
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(methodOverride());
+
 
 /**
  * Import models and controllers
